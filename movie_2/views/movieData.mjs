350163ae729f505cfcf041ef90f5ca0447357f14
ar83had <arshadshmim786@gmail.com>
2024-07-19 23:14:17 +0530

    views/movieData {contain movie data}

---
+++ movie_2/views/movieData.mjs
@@ -0,0 +1,151 @@
+
+const movie=[
+  {
+    "movieId": "1",
+    "movieTittle": "Inception",
+    "ticketPrice": "12.99"
+  },
+  {
+    "movieId": "2",
+    "movieTittle": "The Matrix",
+    "ticketPrice": "10.99"
+  },
+  {
+    "movieId": "3",
+    "movieTittle": "Pulp Fiction",
+    "ticketPrice": "11.99"
+  },
+  {
+    "movieId": "4",
+    "movieTittle": "The Dark Knight",
+    "ticketPrice": "13.99"
+  },
+  {
+    "movieId": "5",
+    "movieTittle": "Fight Club",
+    "ticketPrice": "10.99"
+  },
+  {
+    "movieId": "6",
+    "movieTittle": "Forrest Gump",
+    "ticketPrice": "11.99"
+  },
+  {
+    "movieId": "7",
+    "movieTittle": "The Shawshank Redemption",
+    "ticketPrice": "12.99"
+  },
+  {
+    "movieId": "8",
+    "movieTittle": "The Godfather",
+    "ticketPrice": "13.99"
+  },
+  {
+    "movieId": "9",
+    "movieTittle": "The Lord of the Rings: The Return of the King",
+    "ticketPrice": "14.99"
+  },
+  {
+    "movieId": "10",
+    "movieTittle": "The Dark Knight Rises",
+    "ticketPrice": "13.99"
+  },
+  {
+    "movieId": "11",
+    "movieTittle": "Interstellar",
+    "ticketPrice": "12.99"
+  },
+  {
+    "movieId": "12",
+    "movieTittle": "The Silence of the Lambs",
+    "ticketPrice": "11.99"
+  },
+  {
+    "movieId": "13",
+    "movieTittle": "Forrest Gump",
+    "ticketPrice": "11.99"
+  },
+  {
+    "movieId": "14",
+    "movieTittle": "The Lord of the Rings: The Fellowship of the Ring",
+    "ticketPrice": "14.99"
+  },
+  {
+    "movieId": "15",
+    "movieTittle": "The Godfather: Part II",
+    "ticketPrice": "13.99"
+  },
+  {
+    "movieId": "16",
+    "movieTittle": "The Matrix",
+    "ticketPrice": "10.99"
+  },
+  {
+    "movieId": "17",
+    "movieTittle": "Pulp Fiction",
+    "ticketPrice": "11.99"
+  },
+  {
+    "movieId": "18",
+    "movieTittle": "Schindler's List",
+    "ticketPrice": "12.99"
+  },
+  {
+    "movieId": "19",
+    "movieTittle": "Inception",
+    "ticketPrice": "12.99"
+  },
+  {
+    "movieId": "20",
+    "movieTittle": "Fight Club",
+    "ticketPrice": "10.99"
+  },
+  {
+    "movieId": "21",
+    "movieTittle": "Forrest Gump",
+    "ticketPrice": "11.99"
+  },
+  {
+    "movieId": "22",
+    "movieTittle": "The Dark Knight",
+    "ticketPrice": "13.99"
+  },
+  {
+    "movieId": "23",
+    "movieTittle": "The Shawshank Redemption",
+    "ticketPrice": "12.99"
+  },
+  {
+    "movieId": "24",
+    "movieTittle": "Pulp Fiction",
+    "ticketPrice": "11.99"
+  },
+  {
+    "movieId": "25",
+    "movieTittle": "The Lord of the Rings: The Return of the King",
+    "ticketPrice": "14.99"
+  },
+  {
+    "movieId": "26",
+    "movieTittle": "The Godfather",
+    "ticketPrice": "13.99"
+  },
+  {
+    "movieId": "27",
+    "movieTittle": "The Dark Knight Rises",
+    "ticketPrice": "13.99"
+  },
+  {
+    "movieId": "28",
+    "movieTittle": "Interstellar",
+    "ticketPrice": "12.99"
+  },
+  {
+    "movieId": "29",
+    "movieTittle": "The Silence of the Lambs",
+    "ticketPrice": "11.99"
+  }
+]
+
+
+export{movie}
