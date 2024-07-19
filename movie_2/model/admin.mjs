bbb67a4273cce26f69c923d15d8c9408ebba0ebd
ar83had <arshadshmim786@gmail.com>
2024-07-19 23:03:06 +0530

    model/admin.mjs {signup_store(),login_store(),findMovie()

---
+++ movie_2/model/admin.mjs
@@ -0,0 +1,40 @@
+import {con} from './database.mjs';
+import {movie} from '../views/movieData.mjs'
+
+const fn = 
+{
+    "signup_store": async function(req,res){
+        let query = `select * from movie_admin where email = \"${req.body.email}\"`;
+        let re = await con.query(query);
+        if(re[0].length!=0 || req.body.psw != req.body.psw_repeat)
+            return 0;
+        else
+        {
+            const data =  req.body;
+            query = `insert into movie_admin values (\"${data.email}\",\"${data.psw}\")`;
+            re =  await con.query(query)
+            query = "select * from movie_admin"
+            re = await con.query(query);
+        }
+        return 1;
+    },
+    "login_store":async function(req,res){
+        const data = req.body;
+        let query = `select * from movie_admin where email=\"${data.email}\"`
+        let re = await con.query(query);
+        if(re[0].length===0 || re[0][0].password !== data.psw)
+        {
+            return 0;
+        }
+        else
+            return 1;
+    },
+    "findMovie":async function(id)
+    {
+        console.log(id);
+        let re = movie.find((el)=>{return el.movieId==id})
+        return re;
+    }
+}
+
+export{fn}
