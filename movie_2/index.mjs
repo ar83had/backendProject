dd01c23161dc38a064e099ab7604b3c930f8bcd6
ar83had <arshadshmim786@gmail.com>
2024-07-21 12:38:44 +0530

    index.mjs {insert users route}

--- movie_2/index.mjs
+++ movie_2/index.mjs
@@ -1,10 +1,12 @@
 import express from 'express'
 import {router as main} from './routes/index.mjs';
 import {router as admin} from './routes/admin.mjs';
+import {router as user} from './routes/user.mjs';
 
 const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(`${import.meta.dirname}/views`));
 app.use("/movie/admin",admin);
+app.use("/movie/user",user);
 app.use("/movie",main);
 app.listen(2000);
