8a4474933862a053896aa68a0aacb350eaab804a
ar83had <arshadshmim786@gmail.com>
2024-08-08 20:37:37 +0530

    {index.mjs{complete}}

---
+++ rest_movie/index.mjs
@@ -0,0 +1,32 @@
+import express from 'express'
+import {router as movie} from './router/movie.mjs'
+import {router as login} from './router/login.mjs'
+import {expressjwt} from 'express-jwt';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use("/login",login);
+app.use(expressjwt({secret:"secret",algorithms:['HS256']}));
+app.use((err,req,res,next)=>{
+    if(err.name=="UnauthorizedError")
+    {
+        console.log(err);
+        res.status(401);
+        res.send("Unauthorized");
+        res.end();
+    }
+    else
+    {
+        next();
+    }
+});
+
+app.use("/movie",movie);
+
+app.listen(2000,(err)=>{
+    if(!err)
+        console.log("Server connnected to port number 2000");
+});
